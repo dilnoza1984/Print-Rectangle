2300e4b634d5fae638c740ca7156f53d245a14c8
Dilnoza Akhrarova <121368213+dilnoza1984@users.noreply.github.com>
2023-01-01 17:36:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    
+     // Use nested loops to print out the patterns given in the picture files
+     
+   
+
+for( int i=1; i< 6; i++) {   
+for(int j=1; j<=i; j++)  {   
+System.out.print(j+" ");   
+}   
+System.out.println();   
+}   
+
+    
+System.out.println();
+
+    
+   
+    
+for (int i= 6; i>=1 ; i--) {  
+for (int j=1; j<=i; j++) {  
+System.out.print(j + " ");  
+}  
+System.out.println();  
+}  
+
+
+
+    
+}   
+
+  }
